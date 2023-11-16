0a8c77a6e3a24fd5cd41551c432b62f9902f93ab
il0naa <136170246+il0naa@users.noreply.github.com>
2023-11-16 22:05:41 +0100

    +

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -2,39 +2,45 @@ import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_KZNqPaZLIqp6ZlhsPgRMapnL4XgHVPp9ogre63NkeXfVrj13nob00xbvpsdPBwU7';
-  
-  const BASE_URL = 'https://api.thecatapi.com/v1';
 
-  export function fetchBreeds() {
-    const END_POINT = '/breeds';
-    const PARAMS = new URLSearchParams('');
-    const url = `${BASE_URL}${END_POINT}${PARAMS}`;
-  
-    return axios
-      .get(url)
-      .then(response => {
-        if (response.status === 200) return response;
-        return Promise.reject(response.status);
-      })
-      .catch(error => {
-        errorFn(error);
-      })
-      .finally(loadOn());
-  }
-  
-  export function fetchCatByBreed(breedId) {
-    const END_POINT = '/images/search';
-    const PARAMS = new URLSearchParams({ breed_ids: breedId });
-    const url = `${BASE_URL}${END_POINT}?${PARAMS}`;
-  
-    return axios
-      .get(url)
-      .then(response => {
-        if (response.status === 200) return response;
-        return Promise.reject(response.status);
-      })
-      .catch(error => {
-        errorFn(error);
-      })
-      .finally(loadOn());
-  }
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+export function fetchBreeds() {
+  const END_POINT = '/breeds';
+  const PARAMS = new URLSearchParams('');
+  const url = `${BASE_URL}${END_POINT}${PARAMS}`;
+
+  return axios
+    .get(url)
+    .then(response => {
+      if (response.status === 200) return response;
+      return Promise.reject(response.status);
+    })
+    .catch(error => {
+      errorFn(error);
+    })
+    .finally(() => {
+      // Припускаючи, що loadOn - це функція, яку ви хочете викликати після завершення запиту
+      loadOn();
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  const END_POINT = '/images/search';
+  const PARAMS = new URLSearchParams({ breed_ids: breedId });
+  const url = `${BASE_URL}${END_POINT}?${PARAMS}`;
+
+  return axios
+    .get(url)
+    .then(response => {
+      if (response.status === 200) return response;
+      return Promise.reject(response.status);
+    })
+    .catch(error => {
+      errorFn(error);
+    })
+    .finally(() => {
+      // Припускаючи, що loadOn - це функція, яку ви хочете викликати після завершення запиту
+      loadOn();
+    });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,62 @@
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
 
-import { fetchBreeds } from './cat-api';
-import { fetchCatByBreed } from './cat-api';
-// import '/node_modules/slim-select/dist/slimselect.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { breedMarkup } from './markup'
 
 const selectBreedEl = document.querySelector('.js-breed-select');
 const catInfo = document.querySelector('.js-cat-info');
 const wrapperEl = document.querySelector('.js-wrapper');
 
+firstDownload();
+
+function firstDownload() {
+    wrapperEl.classList.remove('is-hidden');
+    fetchBreeds()
+        .then(data => {
+            const options = data.map(breed => {
+                const option = document.createElement('option');
+                option.textContent = breed.name;
+                option.value = breed.id;
+                return option;
+            });
+
+            selectBreedEl.append(...options);
+
+            new SlimSelect({
+                select: selectBreedEl,
+            });
+        })
+        .catch(onError)
+        .finally(() => {
+            wrapperEl.classList.add('is-hidden');
+        });
+}
+
+selectBreedEl.addEventListener('change', changeBreed);
+
+function changeBreed(e) {
+  let breedIdCat = e.target.value;
+  wrapperEl.classList.remove('is-hidden');
+  fetchCatByBreed(breedIdCat)
+    .then(data => {
+      const markup = breedRender(data);
+      catInfo.innerHTML = markup;
+    })
+    .catch(() => {
+      onError;
+    })
+    .finally(() => {
+      wrapperEl.classList.add('is-hidden');
+    });
+}
+
+function breedRender(breeds) {
+  return breeds.map(breedMarkup).join('');
+}
+
+function onError() {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  }
